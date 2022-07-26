9cfe173ea5741acfc3a338bd52577955dbc06614
Angela Gola <angelagola.ko@gmail.com>
2022-07-26 19:28:13 -0400

    Replies and timestamp working

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
+
+const ReplySchema = new Schema ({
+    // set custom id to avoid confusion with parent comment_id
+    replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+        type: String
+    },
+    writtenBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+},
+{
+    toJSON: {
+        getters:true
+    }
+}
+);
 const CommentSchema = new Schema ({
     writtenBy: {
         type: String
@@ -9,8 +35,25 @@ const CommentSchema = new Schema ({
     },
     CreatedAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    //use to validate data for a reply
+    replies: [ReplySchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
+
+);
+
+//get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
